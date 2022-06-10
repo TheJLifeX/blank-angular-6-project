483bf32a5228ffb0ca97ae84170db1bca688ddc6
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2022-06-10 23:40:09 +0200

    Check if ngx-scroll-position-restoration is compatible with Angular 6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Import NgxScrollPositionRestorationModule
+import { NgxScrollPositionRestorationModule } from 'ngx-scroll-position-restoration';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    /**
+     * Does not seem to work.
+     * 
+     * Error: "Error: StaticInjectorError(AppModule)[NgxScrollPositionRestorationService -> Router]: 
+  StaticInjectorError(Platform: core)[NgxScrollPositionRestorationService -> Router]: 
+    NullInjectorError: No provider for Router!"
+     */
+    NgxScrollPositionRestorationModule.forRoot({
+      debug: true
+    }) // Import NgxScrollPositionRestorationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
